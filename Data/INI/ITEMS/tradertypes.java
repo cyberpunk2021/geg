dd8001aec185b15572877a2f46cf4d6f2f2ef917
Silveryte <framos.df@gmail.com>
2017-01-18 18:53:58 -0300

    Weapon stats should be finished

---
+++ Data/INI/ITEMS/tradertypes.java
@@ -0,0 +1,366 @@
+==========================
+Street trader guns
+==========================
+
+Item "Revolver SW 10 (.38 spc)" Level 1 Price 180
+Item "Revolver SW 10 W (.38 spc)" Level 1 Price 180
+Item "Revolver SW 10 B (.38 spc)" Level 1 Price 180
+Item "Revolver SW 10 WB (.38 spc)" Level 1 Price 180
+Item "Revolver SW 10 BB (.38 spc)" Level 1 Price 180
+Item "Revolver SW 10 GB (.38 spc)" Level 1 Price 180
+Item "Revolver Police Positive (.38 spc)" Level 1 Price 100
+Item "Revolver Police Positive B (.38 spc)" Level 1 Price 100
+Item "Revolver Police Positive W (.38 spc)" Level 1 Price 100
+Item "Revolver Police Positive WB (.38 spc)" Level 1 Price 100
+Item "Revolver Police Positive BB (.38 spc)" Level 1 Price 100
+Item "Revolver Police Positive GB (.38 spc)" Level 1 Price 100
+
+
+Item "Pistol Taurus PT 59 (.380)" Level 1 Price 500
+Item "Pistol Taurus PT 59 W (.380)" Level 1 Price 500
+Item "Pistol Taurus PT 59 B (.380)" Level 1 Price 500
+Item "Pistol Taurus PT 59 Rail (.380)" Level 1 Price 650
+Item "Pistol Taurus PT 59 Rail W (.380)" Level 1 Price 650
+Item "Pistol Taurus PT 59 Rail B (.380)" Level 1 Price 650
+
+Item "Pistol Beretta M9A1 (9mm)" Level 1 Price 990
+Item "Pistol NP-56 (.45 ACP)" Level 1 Price 560
+Item "Pistol NP-29 (9mm)" Level 1 Price 380
+Item "Pistol NP-28 (9mm)" Level 1 Price 500
+Item "Pistol NP-44 (.45 ACP)" Level 1 Price 650
+Item "Pistol NP-26 (9mm)" Level 1 Price 540
+Item "Pistol Norinco M.1911 A1 (.45 ACP)" Level 1 Price 430
+Item "Pistol NZ 75 (9mm)" Level 1 Price 460
+
+Item "Pistol Tokagypt 58 TU-90" Level 1 Price 300
+Item "Pistol TT-33 M213 (9mm)" Level 1 Price 300
+Item "Pistol TT-33 M213 WQ213B54 (9mm)" Level 1 Price 500
+Item "Pistol TT-33 M213 (7.62x25mm)" Level 1 Price 280
+
+Item "Pistol Derringer (22)" Level 1 Price 10
+Item "Pistol Derringer (9mm)" Level 1 Price 30
+Item "Pistol Derringer (40)" Level 1 Price 50
+Item "Pistol Derringer (38)" Level 1 Price 20
+Item "Pistol Derringer (357)" Level 1 Price 60
+
+Item "Pistol PM Type 59 (9x18mm)" Level 1 Price 190
+
+Item "Pistol Ruger 2245 Target" Level 1 Price 120
+Item "Pistol P7 (22)" Level 1 Price 50
+Item "Pistol M.1911 A1 (22)" Level 1 Price 70
+Item "Pistol M.1911 A1 v1 (22)" Level 1 Price 70 
+Item "Pistol M.1911 A1 v2 (22)" Level 1 Price 70
+
+Item "Pistol Ruger 2245 Target" Level 1 Price 120
+Item "Pistol P7 (22)" Level 1 Price 50
+Item "Pistol M.1911 A1 (22)" Level 1 Price 70
+Item "Pistol M.1911 A1 v1 (22)" Level 1 Price 70 
+Item "Pistol M.1911 A1 v2 (22)" Level 1 Price 70
+Item "Pistol P7 (32)" Level 1 Price 60
+Item "Pistol PA-63 32" Level 1 Price 80
+Item "Pistol P7 (380)" Level 1 Price 70
+Item "Pistol PA-63 380" Level 1 Price 100
+Item "Pistol PA-63" Level 1 Price 170
+Item "Pistol HK4 .380" Level 1 Price 110
+Item "Pistol HK4 .32" Level 1 Price 90
+Item "Pistol HK4 .25" Level 1 Price 50
+Item "Pistol HK4 .22" Level 1 Price 40
+Item "Pistol GP_K22" Level 1 Price 100
+Item "Pistol Walter P22 (22)" Level 1 Price 80
+Item "Pistol SIG P210 (22)" Level 1 Price 90
+Item "SIG Sauer Mosquito (22)" Level 1 Price 85
+Item "CZ 75 (22)" Level 1 Price 80
+Item "CZ 75 Shadow (22)" Level 1 Price 200
+Item "Pistol SWMP22" Level 1 Price 170
+Item "Pistol Baby Browning" Level 1 Price 30
+Item "Pistol Colt Mustang (.380 ACP)" Level 1 Price 70
+Item "Pistol Walther 9" Level 1 Price 30
+Item "Pistol Guardian NAA (25)" Level 1 Price 60
+Item "Pistol Guardian NAA (32)" Level 1 Price 80
+Item "Pistol Guardian NAA (380)" Level 1 Price 110
+Item "Pistol P83 .32" Level 1 Price 85
+Item "Pistol P83 .380" Level 1 Price 105
+Item "Pistol Beretta M1935 (32)" Level 1 Price 100
+Item "Pistol Beretta M1935 W (32)" Level 1 Price 100
+Item "Pistol Beretta M1934 W (380)" Level 1 Price 180
+Item "Pistol Beretta M1934 (380)" Level 1 Price 180
+Item "Pistol Sauer 38 (32)" Level 1 Price 90
+Item "Pistol Sauer 38 W (32)" Level 1 Price 90
+Item "Pistol HSC (32)" Level 1 Price 80
+Item "Pistol HSC B (32)" Level 1 Price 80
+Item "Pistol FEG37 (32)" Level 1 Price 100
+Item "Pistol MAB D (32)" Level 1 Price 80
+Item "Pistol MAB D (380)" Level 1 Price 100
+Item "Pistol Micro Deagle (380)" Level 1 Price 90
+Item "Pistol IJ70 (.380)" Level 1 Price 160
+Item "Pistol IJ70 AH17 (.380)" Level 2 Price 190
+Item "Pistol Glock 25 (.380)" Level 1 Price 210
+Item "Pistol Glock 28 (.380)" Level 1 Price 140
+Item "Pistol IMBEL M1911 (380)" Level 2 Price 280
+Item "Pistol Colt Defender (380)" Level 1 Price 130
+Item "Pistol Colt Defender W (380)" Level 1 Price 130
+Item "Pistol PMM (9x18 mm)" Level 2 Price 280
+Item "Pistol MP-448 Skif" Level 1 Price 250
+Item "Pistol P64" Level 1 Price 190
+Item "Pistol P83" Level 1 Price 220
+
+Item "Pistol Kahr K9 (9mm)" Level 1 Price 360
+Item "Pistol Walther P5" Level 1 Price 440
+Item "Pistol BP18 (9mm)" Level 2 Price 490
+Item "Pistol BP18 B (9mm)" Level 2 Price 490
+Item "Pistol IMBEL M1911 (9mm)" Level 2 Price 780
+Item "Pistol IMBEL M1911 (.40)" Level 3 Price 870
+Item "Pistol IMBEL M1911 (.45)" Level 3 Price 850
+Item "Pistol Colt Defender (9mm)" Level 1 Price 460
+Item "Pistol Colt Defender (.40)" Level 2 Price 500
+Item "Pistol Colt Defender W (.40)" Level 2 Price 500
+Item "Pistol Colt Defender W (9mm)" Level 1 Price 460
+Item "Pistol Colt Defender (.45 ACP)" Level 2 Price 490
+Item "Pistol Colt Defender W (.45 ACP)" Level 2 Price 490
+Item "Pistol PT1911 (9mm)" Level 3 Price 840
+Item "Pistol PT1911 (45)" Level 4 Price 930
+Item "Pistol SW908 (9mm)" Level 2 Price 500
+Item "Pistol SW6906 (9mm)" Level 3 Price 590
+Item "Pistol SW5906 (9mm)" Level 3 Price 640
+Item "Pistol SW459 (9mm)" Level 2 Price 590
+
+Item "Pistol Glock 19 (9mm)" Level 3 Price 660
+Item "Pistol Glock 26 (9mm)" Level 2 Price 530
+
+Item "Pistol Beretta T75 (9mm)" Level 2 Price 620
+
+Item "Pistol Beretta M8000 Cougar (9mm)" Level 2 Price 510
+
+Item "Steyr TMP (9x19)" Level 2 Price 1300
+Item "Steyr TMP AUTO (9x19)" Level 3 Price 1900
+
+Item "Pistol Llama (Largo)" Level 1 Price 410
+Item "Pistol Llama Extra (Largo)" Level 1 Price 490
+
+Item "Pistol NP-58 (.40)" Level 1 Price 600
+
+Item "Pistol Kahr K40 (.40)" Level 1 Price 470
+Item "Pistol Browning Hi-Power (.40)" Level 3 Price 670
+
+Item "Pistol Jericho (40)" Level 3 Price 780
+Item "Pistol Jericho W (40)" Level 3 Price 780
+Item "Pistol Jericho (45)" Level 3 Price 760
+Item "Pistol Jericho W (45)" Level 3 Price 760
+
+Item "Pistol Glock 27 (.40)" Level 3 Price 640
+
+Item "Pistol T51K1 (.45 ACP)" Level 2 Price 650
+
+Item "Revolver Nagant" Level 1 Price 160
+
+Item "Revolver Smith & Wesson Classic (.38 spc)" Level 1 Price 130
+Item "Revolver Smith & Wesson 360PD (.38 spc)" Level 1 Price 200
+Item "Revolver Taurus 850 (.38 Special)" Level 1 Price 180
+Item "Revolver Taurus 850 W (.38 Special)" Level 111111 Price 180
+Item "Revolver Taurus 850 H (.38 Special)" Level 111111 Price 180
+Item "Revolver Taurus 850 H W (.38 Special)" Level 111111 Price 180
+Item "Revolver Taurus Model 650 (.357 Magnum)" Level 111111 Price 350
+Item "Revolver Taurus Model 650 W (.357 Magnum)" Level 111111 Price 350
+Item "Revolver Taurus Model 650 H (.357 Magnum)" Level 111111 Price 350
+Item "Revolver Taurus Model 650 H W (.357 Magnum)" Level 111111 Price 350
+Item "Revolver Taurus 82 Stainless (.38 spc)" Level 111111 Price 320
+Item "Revolver Taurus 82 B (.38 spc)" Level 111111 Price 320
+Item "Revolver SW 10 (.38 spc)" Level 111111 Price 200
+Item "Revolver SW 10 W (.38 spc)" Level 111111 Price 200
+Item "Revolver SW 10 B (.38 spc)" Level 111111 Price 200
+Item "Revolver SW 10 WB (.38 spc)" Level 111111 Price 200
+Item "Revolver SW 10 BB (.38 spc)" Level 111111 Price 200
+Item "Revolver SW 10 GB (.38 spc)" Level 111111 Price 200
+
+Item "Revolver Taurus 65 B (.357 Magnum)" Level 111111 Price 450
+Item "Revolver Taurus 66 Stainless (.357 Magnum)" Level 111111 Price 600
+Item "Revolver Taurus 66 B (.357 Magnum)" Level 111111 Price 600
+Item "Revolver Taurus 66 Stainless LB (.357 Magnum)" Level 111111 Price 700
+Item "Revolver Taurus 66 B LB (.357 Magnum)" Level 111111 Price 700
+Item "Revolver Taurus 608 Stainless (.357 Magnum)" Level 111111 Price 900
+Item "Revolver Taurus 608 Stainless SB (.357 Magnum)" Level 111111 Price 800
+Item "Revolver Taurus 608 SB (.357 Magnum)" Level 111111 Price 800
+
+
+Item	"Revolver Taurus 44 Stainless (.44 Magnum)"		600
+Item	"Revolver Taurus 44 Blue (.44 Magnum)"			550
+Item	"Revolver Taurus 65 Stainless (.357 Magnum)"		450
+Item	"Revolver Taurus 608 (.357 Magnum)"			900
+Item	"Revolver Taurus 850 (.38 Special)"			400
+
+Item	"Pistol PM (9x18mm)"				240
+Item	"Pistol Fort 17 (9x18mm)"				300
+Item	"Pistol Fort 17 (380)"				300
+
+Item	"Pistol Type 51 (7.62x25mm)"			220
+Item	"Pistol TT-33 (7.62x25mm)"			200
+Item	"Pistol Tokagypt 58"				200
+Item	"Pistol Zastava M70(d)"				380
+
+Item	"Pistol Beretta 951 (9mm)"			400
+Item	"Pistol Beretta 92S (9mm)"			730
+
+Item	"Pistol Taurus PT 92 (9mm)"			700
+Item	"Pistol Taurus PT100 (.40 S&W)"			850
+Item "Pistol Taurus PT 92 B (9mm)" Level 111111 Price 700
+Item "Pistol Taurus PT 92 W (9mm)" Level 111111 Price 700
+Item "Pistol Taurus PT 92 Rail (9mm)" Level 111111 Price 800
+Item "Pistol Taurus PT 92 Rail B (9mm)" Level 111111 Price 800
+Item "Pistol Taurus PT 92 Rail W (9mm)" Level 111111 Price 800
+Item "Pistol Taurus PT100 W (.40 SW)" Level 111111 Price 850
+Item "Pistol Taurus PT100 B (.40 SW)" Level 111111 Price 850
+
+Item	"Pistol Star B Super (9mm)" Level 111111 Price                     370
+Item	"Pistol Ballester-Molina (.45 ACP)"		410
+
+Item "Pistol HSC (380)" Level 111111 Price 110
+Item "Pistol HSC B (380)" Level 111111 Price 110
+Item "Pistol FEG37 (380)" Level 111111 Price 120
+
+
+
+Item	"Submachine-gun Micro UZI (9mm)"		1050
+Item	"Submachine-gun Micro UZI (9mm) folded"		1050
+Item	"Submachine-gun Mini UZI (9mm)"			1200
+Item	"Submachine-gun Mini UZI (9mm) folded"		1200
+
+Item	"Submachine-gun HK MP5KA4 (9mm)"		1300
+Item	"Submachine-gun HK MP5PDW (9mm)"		1400
+Item	"Submachine-gun HK MP5PDW (9mm) folded"		1400
+
+Item	"Submachine-gun MP40 (9mm)"			500
+Item	"Submachine-gun MP40 (9mm) folded"		500
+Item	"Submachine-gun MAT49 (9mm)"			650
+Item	"Submachine-gun MAT49 (9mm) folded"		650
+Item	"Submachine-gun Sterling L2A3 (9mm)"		580
+Item	"Submachine-gun Sterling L2A3 (9mm) folded"	580
+
+Item	"Submachine-gun M3A1 (.45 ACP)"			400
+Item	"Submachine-gun M3A1 (.45 ACP) folded"		400
+
+Item	"Submachine-gun MAC-10 (.45 ACP)"		900
+Item	"Submachine-gun MAC-10 (.45 ACP) folded"	900
+
+
+Item "Submachine-gun Type 85 (7.62x25mm)" Level 111111 Price 160
+Item "Submachine-gun Type 85 (7.62x25mm) folded" Level 111111 Price 160
+Item "Submachine-gun Type 79 (7.62x25mm)" Level 111111 Price 220
+Item "Submachine-gun Type 79 (7.62x25mm) folded" Level 111111 Price 220
+Item "Submachine-gun K50M (7.62x25mm)" Level 111111 Price 400
+Item "Submachine-gun K50M (7.62x25mm) folded" Level 111111 Price 400
+
+Item "Submachine-gun M3A1 (9mm)" Level 111111 Price 300
+Item "Submachine-gun M3A1 (9mm) folded" Level 111111 Price 300
+
+
+Item "Submachine-gun Sten (9mm)" Level 111111 Price 190
+Item "Carl Gustaf Kpist M45 (9mm)" Level 111111 Price 780
+Item "Carl Gustaf Kpist M45 (9mm) folded" Level 111111 Price 780
+
+Item "MAT-49 (7.62x25)" Level 111111 Price 700
+Item "MAT-49 (7.62x25) folded" Level 111111 Price 700
+
+Item "Carl Gustaf Kpist M50 (9mm)" Level 111111 Price 700
+Item "Carl Gustaf Kpist M50 (9mm) folded" Level 111111 Price 700
+Item "Carl Gustaf Kpist INA (9mm)" Level 111111 Price 800
+Item "Carl Gustaf Kpist INA (9mm) folded" Level 111111 Price 800
+Item "Carl Gustaf Kpist M45 (45)" Level 111111 Price 850
+Item "Carl Gustaf Kpist M45 (45) folded" Level 111111 Price 850
+
+
+Item "Submachine-gun VZ82 (380)" Level 111111 Price 640
+Item "Submachine-gun VZ82 (380) folded" Level 111111 Price 640
+Item "Submachine-gun VZ82 (9x19mm)" Level 111111 Price 900
+Item "Submachine-gun VZ82 (9x19mm) folded" Level 111111 Price 900
+
+Item "Submachine-gun kedr" Level 111111 Price 910
+Item "Submachine-gun kedr folded" Level 111111 Price 910
+
+Item	"Submachine-gun Vz61 (7.65mm)"			620
+Item	"Submachine-gun Vz61 (7.65mm) folded"		620
+
+Item	"Submachine-gun Type 54 (7.62x25mm)"		500
+Item	"Submachine-gun Type 54 (7.62x25mm) folded"	500
+Item	"Submachine-gun Vz82 (9x18mm)"			740
+Item	"Submachine-gun Vz82 (9x18mm) folded"		740
+
+Item "Submachine-gun KG-99 (9mm)" Level 111111 Price 700
+Item "Submachine-gun KG-99 (9mm) folded" Level 111111 Price 700
+Item "Submachine-gun KG-99 AUTO (9mm)" Level 111111 Price 1100
+Item "Submachine-gun KG-99 AUTO (9mm) folded" Level 111111 Price 1100
+Item "Submachine-gun TEC-9 (9mm)" Level 111111 Price 600
+Item "Submachine-gun TEC-9 AUTO (9mm)" Level 111111 Price 900
+Item "Submachine-gun TEC-9 Mini (9mm)" Level 111111 Price 500
+Item "Submachine-gun TEC-9 AUTO Mini (9mm)" Level 111111 Price 800
+Item "Submachine-gun AB-10 (9mm)" Level 111111 Price 400
+Item "Submachine-gun AB-10 AUTO (9mm)" Level 111111 Price 700
+
+Item "A2000" Level 111111 Price 1000
+
+Item "Submachine-gun RATMIL md96 (9mm)" Level 111111 Price 900
+Item "Submachine-gun RATMIL md96 (9mm) folded" Level 111111 Price 900
+Item "Submachine-gun MAC-10 (380)" Level 111111 Price 800
+Item "Submachine-gun MAC-10 (380) folded" Level 111111 Price 800
+Item "Submachine-gun Mini MAC-11 (9mm)" Level 111111 Price 740
+Item "Submachine-gun MAC-11 (9mm)" Level 111111 Price 950
+Item "Submachine-gun MAC-11 (9mm) folded" Level 111111 Price 950
+
+Item	"Shotgun CoachGun SawedOff (12ga)"			100
+Item	"Shotgun CoachGun (12ga)"				200
+Item	"Shotgun Mosberg 590 Crusader (12ga)"			360
+
+Item	"Rifle Mosin Sawedoff (7.62x54 mm)"			100
+
+Item	"Shotgun Ithaca 37 (12ga)"				450
+
+Item "Shotgun CBC PUMP PGRIP (12ga)" Level 111111 Price 350
+Item "Shotgun CBC PUMP PGRIP LB (12ga)" Level 111111 Price 380
+
+Item "Baikal 133 (12ga)" Level 111111 Price 420
+Item "Shotgun Boito PUMP (12ga)" Level 111111 Price 460
+Item "Shotgun CBC PUMP (12ga)" Level 111111 Price 520
+Item "Shotgun CBC PUMP LB (12ga)" Level 111111 Price 540
+Item "Shotgun TOZ 87 (12ga)" Level 111111 Price 500
+Item "Shotgun Remington 11-87 SB (12ga)" Level 111111 Price 900
+Item "Mossberg 500 v2" Level 111111 Price 300
+Item "Shotgun CoachGun (20ga)" Level 111111 Price 160
+Item "Sgun2Bs (20ga)" Level 111111 Price 80
+Item "Sgun2Bs (20ga)" Level 111111 Price 80
+Item "ScatterGun" Level 111111 Price 140
+Item "ScatterGun 20ga" Level 111111 Price 120
+Item "Shotgun TOZ34" Level 111111 Price 300
+Item "Shotgun TOZ34 20ga" Level 111111 Price 220
+Item "Shotgun Ij 27" Level 111111 Price 320
+Item "Shotgun Ij 27 20ga" Level 111111 Price 240
+Item "Shotgun-2B (12ga)" Level 111111 Price 200
+Item "Shotgun-2B (20ga)" Level 111111 Price 160
+Item "CoachGun2" Level 111111 Price 330
+Item "Shotgun Silverhawk (12ga)" Level 111111 Price 500
+Item "Shotgun Silverhawk (20ga)" Level 111111 Price 400
+Item "Serbu Shorty" Level 111111 Price 340
+Item "Serbu Shorty 20ga" Level 111111 Price 260
+Item "Mosberg 590 Crusader (20ga)" Level 111111 Price 300
+Item "Mosberg 590 Crusader (410)" Level 111111 Price 210
+Item "Shotgun Ithaca 37 (20ga)" Level 111111 Price 320
+Item "Baikal (12ga)" Level 111111 Price 530
+Item "Winchester 1892 trapper 12ga" Level 111111 Price 240
+Item "Winchester 1892 carbine 12ga" Level 111111 Price 370
+Item "Shotgun 1897 (12ga)" Level 111111 Price 550
+Item "Shotgun Auto 5 (12ga)" Level 111111 Price 690
+Item "Shotgun Auto 5 (20ga)" Level 111111 Price 470
+Item "Shotgun 1912 (12ga)" Level 111111 Price 610
+Item "Shotgun 1912 (20ga)" Level 111111 Price 420
+Item "Shotgun Benelli M1 (20ga)" Level 111111 Price 850
+Item "Shotgun Benelli M2 (20ga)" Level 111111 Price 800
+Item "TOZ194 unstock (12ga)" Level 111111 Price 900
+
+
+
+
+
+
+
+
+
+
+
